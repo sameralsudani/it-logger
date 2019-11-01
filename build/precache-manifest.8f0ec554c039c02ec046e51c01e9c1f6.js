14849b9e1a48a9f54c0ffadaa20800344e9fd9cf
Samer <samerxxxalsudani@gmail.com>
2019-11-01 10:03:14 -0500

    Second Commit

---
+++ build/precache-manifest.8f0ec554c039c02ec046e51c01e9c1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd10540704a1a31a7bf2e01fe390f09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "76555932f26711f682f2",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/static/js/2.fadd1756.chunk.js"
+  },
+  {
+    "revision": "76555932f26711f682f2",
+    "url": "/static/js/main.440ffb6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
